d44ba54665d6778161601611fba978514aa1458c
RulleR <rullerrbl@mail.ru>
2022-04-30 16:50:52 +0300

    v1

--- src/dev/igorekudashev/Converter.java
+++ src/dev/igorekudashev/Converter.java
@@ -1,14 +1,12 @@
 package dev.igorekudashev;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.TreeMap;
 
 public class Converter {
 
-    private static final String[] romanNumbers = {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "XI", "X"};
-
-    private static final TreeMap<Integer, String> arabicToRomanMap = new TreeMap<>() {{
+    private static final List<String> romanNumerals = List.of("I", "II", "III", "IV", "V", "VI", "VII", "VIII", "XI", "X");
+    private static final TreeMap<Integer, String> numeralsMap = new TreeMap<>() {{
         put(100, "C");
         put(90, "XC");
         put(50, "L");
@@ -21,21 +19,16 @@ public class Converter {
     }};
 
     public static int romanToArabic(String romanNumber) {
-        List<String> test = new ArrayList<>();
-
-
-        for (int i = 0; i < romanNumbers.length; i++) {
-            if (romanNumbers[i].equals(romanNumber.toUpperCase())) {
-                return i + 1;
-            }
-        }
-        throw new IllegalArgumentException();
+        int index = romanNumerals.indexOf(romanNumber);
+        if (index == -1) throw new IllegalArgumentException();
+        else return index + 1;
     }
 
     public static String arabicToRoman(int arabicNumber) {
-        int floorValue = arabicToRomanMap.floorKey(arabicNumber);
+        if (arabicNumber <= 1) throw new IllegalArgumentException();
+        int floorValue = numeralsMap.floorKey(arabicNumber);
         return arabicNumber == floorValue
-                ? arabicToRomanMap.get(arabicNumber)
-                : arabicToRomanMap.get(floorValue) + arabicToRoman(arabicNumber - floorValue);
+                ? numeralsMap.get(arabicNumber)
+                : numeralsMap.get(floorValue) + arabicToRoman(arabicNumber - floorValue);
     }
 }
--- src/dev/igorekudashev/Main.java
+++ src/dev/igorekudashev/Main.java
@@ -5,29 +5,20 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println(calc(scanner.nextLine()));
+        System.out.println(calc(new Scanner(System.in).nextLine()));
     }
 
     public static String calc(String input) {
-        String[] values = input.split("\\s", 3);
+        String[] parts = input.split("\\s", 3);
         try {
-            return String.valueOf(apply(
-                    Integer.parseInt(values[0]),
-                    Integer.parseInt(values[2]),
-                    values[1]));
+            return String.valueOf(apply(Integer.parseInt(parts[0]), Integer.parseInt(parts[2]), parts[1]));
         } catch (NumberFormatException ignored) {
-            return Converter.arabicToRoman(apply(
-                    Converter.romanToArabic(values[0]),
-                    Converter.romanToArabic(values[2]),
-                    values[1]));
+            return Converter.arabicToRoman(apply(Converter.romanToArabic(parts[0]), Converter.romanToArabic(parts[2]), parts[1]));
         }
     }
 
     private static int apply(int var1, int var2, String operation) {
-        if (var1 < 1 || var1 > 10 || var2 < 1 || var2 > 10) {
-            throw new IllegalArgumentException();
-        }
+        if (var1 < 1 || var1 > 10 || var2 < 1 || var2 > 10) throw new IllegalArgumentException();
         return switch (operation) {
             case "+" -> var1 + var2;
             case "-" -> var1 - var2;
