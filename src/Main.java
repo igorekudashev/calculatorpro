3901aef9b61f5ae77062839d41241a565b86f47e
RulleR <rullerrbl@mail.ru>
2022-06-11 17:58:39 +0300

    Жесть x2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.util.*;
-import java.util.function.IntBinaryOperator;
+import java.util.function.*;
 public class Main {
+    private static final List<Function<Integer, String>> arabicToRomanWrapper = new ArrayList<>() {{add((arab) -> arab.equals(map.floorKey(arab)) ? map.get(arab) : map.get(map.floorKey(arab)) + get(0).apply(arab - map.floorKey(arab)));}};
     private static final List<String> romans = List.of("I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X");
     private static final Map<String, IntBinaryOperator> operations = Map.of("+", Integer::sum, "-", (a, b) -> a - b, "*", (a, b) -> a * b, "/", (a, b) -> a / b);
     private static final TreeMap<Integer, String> map = new TreeMap<>(Map.of(100, "C", 90, "XC", 50, "L", 40, "XL", 10, "X", 9, "IX", 5, "V", 4, "IV", 1, "I"));
@@ -10,16 +11,11 @@ public class Main {
     public static String calc(String input) {
         String[] parts = input.split("\\s", 3);
         try {
-            return String.valueOf(apply(Integer.parseInt(parts[0]), Integer.parseInt(parts[2]), parts[1]));
+            if (Integer.parseInt(parts[0]) < 1 || Integer.parseInt(parts[0]) > 10 || Integer.parseInt(parts[2]) < 1 || Integer.parseInt(parts[2]) > 10) throw new IllegalArgumentException();
+            return String.valueOf(operations.get(parts[1]).applyAsInt(Integer.parseInt(parts[0]), Integer.parseInt(parts[2])));
         } catch (NumberFormatException ignored) {
-            return arabicToRoman(apply(romans.indexOf(parts[0]) + 1, romans.indexOf(parts[2]) + 1, parts[1]));
+            if (!romans.contains(parts[0]) || !romans.contains(parts[2])) throw new IllegalArgumentException();
+            return arabicToRomanWrapper.get(0).apply(operations.get(parts[1]).applyAsInt(romans.indexOf(parts[0]) + 1, romans.indexOf(parts[2]) + 1));
         }
-    }
-    private static int apply(int a, int b, String operation) {
-        if (a < 1 || a > 10 || b < 1 || b > 10) throw new IllegalArgumentException();
-        return operations.get(operation).applyAsInt(a, b);
-    }
-    private static String arabicToRoman(int arab) {
-        return arab == map.floorKey(arab) ? map.get(arab) : map.get(map.floorKey(arab)) + arabicToRoman(arab - map.floorKey(arab));
     }
 }
