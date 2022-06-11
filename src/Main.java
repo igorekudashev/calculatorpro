67fb84be91b479c67caa709f94dcaaa940ebb621
RulleR <rullerrbl@mail.ru>
2022-06-11 17:18:53 +0300

    Жесть

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
 import java.util.*;
+import java.util.function.IntBinaryOperator;
 public class Main {
-    private static final TreeMap<Integer, String> numeralsMap = new TreeMap<>() {{
-        int[] arabs = {100, 90, 50, 40, 10, 9, 5, 4, 1};
-        String[] romans = {"C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-        for (int i = 0; i < 9; i++) put(arabs[i], romans[i]);
-    }};
+    private static final List<String> romans = List.of("I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X");
+    private static final Map<String, IntBinaryOperator> operations = Map.of("+", Integer::sum, "-", (a, b) -> a - b, "*", (a, b) -> a * b, "/", (a, b) -> a / b);
+    private static final TreeMap<Integer, String> map = new TreeMap<>(Map.of(100, "C", 90, "XC", 50, "L", 40, "XL", 10, "X", 9, "IX", 5, "V", 4, "IV", 1, "I"));
     public static void main(String[] args) {
         System.out.println(calc(new Scanner(System.in).nextLine()));
     }
@@ -13,27 +12,14 @@ public class Main {
         try {
             return String.valueOf(apply(Integer.parseInt(parts[0]), Integer.parseInt(parts[2]), parts[1]));
         } catch (NumberFormatException ignored) {
-            return arabicToRoman(apply(romanToArabic(parts[0]), romanToArabic(parts[2]), parts[1]));
+            return arabicToRoman(apply(romans.lastIndexOf(parts[0]) + 1, romans.lastIndexOf(parts[2]) + 1, parts[1]));
         }
     }
-    private static int apply(int var1, int var2, String operation) {
-        if (var1 < 1 || var1 > 10 || var2 < 1 || var2 > 10) throw new IllegalArgumentException();
-        return switch (operation) {
-            case "+" -> var1 + var2;
-            case "-" -> var1 - var2;
-            case "*" -> var1 * var2;
-            case "/" -> var1 / var2;
-            default -> throw new IllegalArgumentException();
-        };
+    private static int apply(int a, int b, String operation) {
+        if (a < 1 || a > 10 || b < 1 || b > 10) throw new IllegalArgumentException();
+        return operations.get(operation).applyAsInt(a, b);
     }
-    private static int romanToArabic(String romanNumber) {
-        int index = List.of("I", "II", "III", "IV", "V", "VI", "VII", "VIII", "XI", "X").indexOf(romanNumber);
-        if (index == -1) throw new IllegalArgumentException();
-        else return index + 1;
-    }
-    private static String arabicToRoman(int arabicNumber) {
-        if (arabicNumber <= 0) throw new IllegalArgumentException();
-        int floorValue = numeralsMap.floorKey(arabicNumber);
-        return arabicNumber == floorValue ? numeralsMap.get(arabicNumber) : numeralsMap.get(floorValue) + arabicToRoman(arabicNumber - floorValue);
+    private static String arabicToRoman(int arab) {
+        return arab == map.floorKey(arab) ? map.get(arab) : map.get(map.floorKey(arab)) + arabicToRoman(arab - map.floorKey(arab));
     }
 }
