3410d79def0f4dca45f24af2bde0c2de625e9855
igorekudashev <igorkudashevmine@gmail.com>
2022-06-19 13:51:55 +0300

    v8 (13)

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,13 @@
 import java.util.*;
 import java.util.function.*;
 public class Main {
-    private static final List<Function<Integer, String>> arabicToRomanWrapper = new ArrayList<>() {{add((arab) -> arab.equals(map.floorKey(arab)) ? map.get(arab) : map.get(map.floorKey(arab)) + get(0).apply(arab - map.floorKey(arab)));}};
-    private static final List<String> romans = List.of("I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X");
+    private static final List<String> romans = List.of("I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX", "XXI", "XXII", "XXIII", "XXIV", "XXV", "XXVI", "XXVII", "XXVIII", "XXIX", "XXX", "XXXI", "XXXII", "XXXIII", "XXXIV", "XXXV", "XXXVI", "XXXVII", "XXXVIII", "XXXIX", "XL", "XLI", "XLII", "XLIII", "XLIV", "XLV", "XLVI", "XLVII", "XLVIII", "XLIX", "L", "LI", "LII", "LIII", "LIV", "LV", "LVI", "LVII", "LVIII", "LIX", "LX", "LXI", "LXII", "LXIII", "LXIV", "LXV", "LXVI", "LXVII", "LXVIII", "LXIX", "LXX", "LXXI", "LXXII", "LXXIII", "LXXIV", "LXXV", "LXXVI", "LXXVII", "LXXVIII", "LXXIX", "LXXX", "LXXXI", "LXXXII", "LXXXIII", "LXXXIV", "LXXXV", "LXXXVI", "LXXXVII", "LXXXVIII", "LXXXIX", "XC", "XCI", "XCII", "XCIII", "XCIV", "XCV", "XCVI", "XCVII", "XCVIII", "XCIX", "C");
     private static final Map<String, IntBinaryOperator> operations = Map.of("+", Integer::sum, "-", (a, b) -> a - b, "*", (a, b) -> a * b, "/", (a, b) -> a / b);
-    private static final TreeMap<Integer, String> map = new TreeMap<>(Map.of(100, "C", 90, "XC", 50, "L", 40, "XL", 10, "X", 9, "IX", 5, "V", 4, "IV", 1, "I"));
     public static void main(String[] args) {
         System.out.println(calc(new Scanner(System.in).nextLine()));
     }
     public static String calc(String input) {
         String[] parts = input.split("\\s", 3);
-        try {
-            if (Integer.parseInt(parts[0]) < 1 || Integer.parseInt(parts[0]) > 10 || Integer.parseInt(parts[2]) < 1 || Integer.parseInt(parts[2]) > 10) throw new IllegalArgumentException();
-            return String.valueOf(operations.get(parts[1]).applyAsInt(Integer.parseInt(parts[0]), Integer.parseInt(parts[2])));
-        } catch (NumberFormatException ignored) {
-            if (!romans.contains(parts[0]) || !romans.contains(parts[2])) throw new IllegalArgumentException();
-            return arabicToRomanWrapper.get(0).apply(operations.get(parts[1]).applyAsInt(romans.indexOf(parts[0]) + 1, romans.indexOf(parts[2]) + 1));
-        }
+        return parts[0].matches("[1-9]|10") && parts[2].matches("[1-9]|10") ? String.valueOf(operations.get(parts[1]).applyAsInt(Integer.parseInt(parts[0]), Integer.parseInt(parts[2]))) : romans.contains(parts[0]) && romans.contains(parts[2]) && romans.indexOf(parts[0]) <= 10 && romans.indexOf(parts[2]) <= 10 ? romans.get(operations.get(parts[1]).applyAsInt(romans.indexOf(parts[0]) + 1, romans.indexOf(parts[2]) + 1) - 1) : romans.get(romans.indexOf("."));
     }
 }
